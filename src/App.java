4cd698dd56f949aab6d045284bc59e14d3ccf787
Daiskz <gabriel.matsubara@gmail.com>
2022-10-02 13:42:15 -0300

    criação do projeto

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Double valorHora;
+        Double horasTrabalhadas;
+        Double valorMes;
+        System.out.println("Digite o valor que você ganha por hora: ");
+        valorHora = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor que você ganha por hora"));
+        System.out.println("Digite as horas trabalhadas no mês: ");
+        horasTrabalhadas = Double.parseDouble(JOptionPane.showInputDialog("Digite as horas trabalhadas no mês"));
+        valorMes = (valorHora * horasTrabalhadas);
+        System.out.println("O valor do salário no mês é: " + valorMes);
+    }
+}
